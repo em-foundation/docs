3778e03bd61389b9fd82b5729632f67901db5808
bob frankel <bob@biosbob.biz>
2024-01-26 17:01:16 -0800

    RELEASE

--- site/populate.sh
+++ site/populate.sh
@@ -3,4 +3,4 @@
 (cd ../../em-bios/em.bench; em-cli-dev markdown -o ../../em-docs/docs/cargo)
 (cd ../../em-bios/em.core; em-cli-dev markdown -o ../../em-docs/docs/cargo)
 (cd ../../em-bios/em.docs; em-cli-dev markdown -o ../../em-docs/docs/cargo)
-(cd ../../em-bios/ti.cc23xx; em-cli-dev markdown -o ../../em-docs/docs/cargo)
+(cd ../../em-bios/ti.cc23xx; em-cli-dev -S ti.cc23xx/default markdown -o ../../em-docs/docs/cargo)
